76f8cf7be61545f20705ae73568d73ad8317b7a9
p18011704 <corentin.plee@etu.univ-etu.fr>
2019-09-25 09:08:00 +0200

    yes

---
+++ td2php.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ceci est une page de test avec des balises PHP</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <h2>Page de test</h2>
+
+        <p>
+            Cette page contient du code HTML avec des balises PHP.<br />
+    </body>
+</html>
